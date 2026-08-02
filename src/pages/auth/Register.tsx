11117f9331f16fff5915c9a3e80c0a0309fb8560
chen shun <3081876663@qq.com>
2026-08-02 09:25:39 +0800

    1

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,18 +1,69 @@
 // import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Button, Input, Space } from 'antd';
-import "../../css/auth/register.scss";
+import type { FormProps } from 'antd';
+import { Button, Input, Form } from 'antd';
+import '../../css/auth/register.scss';
+import { userApi } from '../../api/user';
 
-const Register = () => {
-  return <div className='page-wrap'>
-    <Space vertical>
-      <Input placeholder="请输入账号" />
-      <Input placeholder="请输入密码" />
-      <Input.Password placeholder="input password" />
-    </Space>
-    <Button type="primary" block className='sub-btn'>
-      提交
-    </Button>
-  </div>;
+type FieldType = {
+  username?: string;
+  password?: string;
+  email?: string;
 };
 
-export default Register;
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+  console.log('Success:', values);
+  userApi.createUser(values).then(res=>{
+    console.log(values, '111')
+  })
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+const Register = () => {
+  return (
+    <div className='page-wrap'>
+      <Form
+        name='basic'
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete='off'
+      >
+        <Form.Item<FieldType>
+          label='Username'
+          name='username'
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label='Email'
+          name='email'
+          rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label='Password'
+          name='password'
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
+}
+
+export default Register
