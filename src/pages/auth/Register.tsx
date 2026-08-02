bb14b660d3aa8e527374e2cdd5e105404eb84c60
chen shun <3081876663@qq.com>
2026-08-02 09:05:09 +0800

    1

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,6 +1,18 @@
-// src/pages/auth/Register.tsx
+// import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { Button, Input, Space } from 'antd';
+import "../../css/auth/register.less";
+
 const Register = () => {
-  return <div>注册</div>;
+  return <div className='page-wrap'>
+    <Space vertical>
+      <Input placeholder="请输入账号" />
+      <Input placeholder="请输入密码" />
+      {/* <Input.Password placeholder="input password" /> */}
+    </Space>
+    <Button type="primary" block>
+      提交
+    </Button>
+  </div>;
 };
 
 export default Register;
